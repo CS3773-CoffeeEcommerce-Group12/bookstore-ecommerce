6b82066ecc2048f91afcf4b57eb0078635b783b2
Alanna Matundan <rosealanna18@gmail.com>
2025-11-02 23:34:34 -0600

    delete next

--- .next/dev/static/chunks/bookstore-ecommerce_app_favicon_ico_mjs_b2c83acf._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/63158_next_dist_9c1a95ed._.js"
-],
-    source: "dynamic"
-});
